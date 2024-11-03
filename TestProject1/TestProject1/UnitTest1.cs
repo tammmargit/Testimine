0d39538f3a3c4e3bb6439e8e9f9b60a185f81438
LAPTOP-EF8HT3HV\ruthp <margit.tammeorg@gmail.com>
2024-11-03 14:46:39 +0200

    one pass, one fail

---
+++ TestProject1/TestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace selenium
+{
+    public class Tests
+    {
+        private IWebDriver? driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.co.in");
+        }
+
+        [TearDown]
+        public void Shutdown()
+        {
+            driver?.Dispose();
+        }
+
+        [Test]
+        public void Should_SearchButton_Exist()
+        {
+            //pass
+            var searchButton = driver.FindElement(By.Name("btnK"));
+            Assert.NotNull(searchButton, "Search button should exist on the Google homepage.");
+        }
+
+        [Test]
+        public void Should_Fail_NonexistentElement()
+        {
+            // fail
+            var nonExistentElement = driver.FindElement(By.Id("nonexistent-id"));
+            Assert.NotNull(nonExistentElement, "This element does not exist, so this test should fail.");
+        }
+    }
+}
